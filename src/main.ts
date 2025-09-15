0ab55d0be3bcaf35c49b85f22b3e80ad29b5208b
ResyaR <resyarizqi387@gmail.com>
2025-09-15 13:36:48 +0700

    feat: configure for Vercel serverless deployment

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  setupSwagger(app);
-  await app.listen(process.env.PORT ?? 5000);
+const expressApp = express();
+let cachedServer;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    app.enableCors();
+    setupSwagger(app);
+    await app.init();
+    cachedServer = expressApp;
+  }
+  return cachedServer;
 }
-bootstrap();
+
+export default async function handler(req, res) {
+  const server = await bootstrapServer();
+  server(req, res);
+}
